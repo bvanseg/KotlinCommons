5646cc3dd0d0f19dce8a3b6921a4fea21deced7f
Boston Vanseghi <bvanseg@gmail.com>
2019-09-29 23:17:26 -0400

    Added lists extension that allows checking for matches between two lists.

---
+++ src/main/kotlin/kcommons/collections/ListExtensions.kt
@@ -0,0 +1,10 @@
+package kcommons.collections
+
+fun <E> List<E>.containsAny(list: List<E>): Boolean {
+    list.forEach {
+        if(this.contains(it)) {
+            return true
+        }
+    }
+    return false
+}
