aadf01a87c0339a4c65fd549c0c1be3d2d9fc085
Boston Vanseghi <bvanseg@gmail.com>
2019-09-29 23:15:29 -0400

    Added a clampOrNull function for numbers.

--- src/main/kotlin/kcommons/numbers/NumberUtils.kt
+++ src/main/kotlin/kcommons/numbers/NumberUtils.kt
@@ -5,3 +5,12 @@ fun <T: Number> clamp(value: T, min: T, max: T): T  = when {
     value.toDouble() > max.toDouble() -> max
     else -> value
 }
+
+fun <T: Number> clampOrNull(value: T?, min: T, max: T): T? {
+    if(value == null) return null
+    return when {
+        value.toDouble() < min.toDouble() -> min
+        value.toDouble() > max.toDouble() -> max
+        else -> value
+    }
+}
