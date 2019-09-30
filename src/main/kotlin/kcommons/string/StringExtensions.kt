ad901d0342080902e9841bc46a875a71723aa681
Boston Vanseghi <bvanseg@gmail.com>
2019-09-29 23:16:09 -0400

    Added string joining and separator replacing for strings.

--- src/main/kotlin/kcommons/string/StringExtensions.kt
+++ src/main/kotlin/kcommons/string/StringExtensions.kt
@@ -1,2 +1,17 @@
 package kcommons.string
 
+fun List<String>.joinStrings(startIndex: Int = 0, endIndex: Int = -1): String {
+    val sb = StringBuilder()
+    val start = kotlin.math.max(startIndex, 0)
+    val end = if (endIndex < 0) this.size else kotlin.math.min(endIndex, this.size)
+    if (start > end)
+        throw IllegalArgumentException("Start index ($start) must not be greater than the end index ($end)")
+    (start until end).forEach {
+        if (it > start)
+            sb.append(" ")
+        sb.append(this[it])
+    }
+    return sb.toString()
+}
+
+fun String.replaceSeparators(): String = this.replace(",", "").replace("_", "")
