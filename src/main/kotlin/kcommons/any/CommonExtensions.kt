4c9e36ac1e988421ec546964e16ec90de01d8a08
Boston Vanseghi <bvanseg@gmail.com>
2019-09-29 23:15:09 -0400

    Added logging and logging extensions.

---
+++ src/main/kotlin/kcommons/any/CommonExtensions.kt
@@ -0,0 +1,9 @@
+package kcommons.any
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * Allows any class to get a logger of itself.
+ */
+fun Any.getLogger(): Logger = LoggerFactory.getLogger(this::class.java)
