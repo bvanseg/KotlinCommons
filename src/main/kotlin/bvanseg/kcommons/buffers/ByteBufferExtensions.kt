341158a2cab631bb3e3144bebf5a2666742cb30d
Boston Vanseghi <bvanseg@gmail.com>
2019-10-04 00:24:23 -0400

    Updated ByteBuffer extension methods.

--- src/main/kotlin/bvanseg/kcommons/buffers/ByteBufferExtensions.kt
+++ src/main/kotlin/bvanseg/kcommons/buffers/ByteBufferExtensions.kt
@@ -4,17 +4,17 @@ import bvanseg.kcommons.projects.Version
 import java.nio.ByteBuffer
 import java.util.*
 
-fun ByteBuffer.getBytes(buffer: ByteBuffer): ByteArray = this.getBytes(this, this.short.toInt())
+fun ByteBuffer.getBytes(): ByteArray = getBytes(this.short.toInt())
 
-fun ByteBuffer.getBytes(buffer: ByteBuffer, num: Int): ByteArray {
+fun ByteBuffer.getBytes(num: Int): ByteArray {
     val bytes = ByteArray(num)
-    buffer.get(bytes)
+    this.get(bytes)
     return bytes
 }
 
 fun ByteBuffer.getUUID(): UUID = UUID(long, long)
 
-fun ByteBuffer.getString(): String = String(this.getBytes(this))
+fun ByteBuffer.getString(): String = String(this.getBytes())
 
 fun ByteBuffer.getVersion(): Version = Version(int, int, int)
 
