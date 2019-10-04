bf334b5ac328758f62821de2c187d8f212c9c2a6
Boston Vanseghi <bvanseg@gmail.com>
2019-10-03 23:26:59 -0400

    Added packet extensions for SimpleNet 1.5.1.

---
+++ src/main/kotlin/bvanseg/kcommons/simplenet/packet/PacketExtensions.kt
@@ -0,0 +1,26 @@
+package bvanseg.kcommons.simplenet.packet
+
+import bvanseg.kcommons.projects.Version
+import com.github.simplenet.packet.Packet
+import java.util.*
+
+/**
+ * @author Bright_Spark
+ */
+
+fun Packet.putBytes(byteArr: ByteArray) {
+    byteArr.forEach {
+        this.putByte(it.toInt())
+    }
+}
+
+fun Packet.putUUID(uuid: UUID) {
+    this.putLong(uuid.mostSignificantBits)
+    this.putLong(uuid.leastSignificantBits)
+}
+
+fun Packet.putVersion(version: Version) {
+    putInt(version.major)
+    putInt(version.minor)
+    putInt(version.patch)
+}
