cc3ce0f73577bde50f4533c744f43ca963016e1f
Boston Vanseghi <bvanseg@gmail.com>
2019-10-04 00:31:40 -0400

    Added JOML-related extensions for Packet and ByteBuffer.

--- src/main/kotlin/bvanseg/kcommons/buffers/ByteBufferExtensions.kt
+++ src/main/kotlin/bvanseg/kcommons/buffers/ByteBufferExtensions.kt
@@ -1,6 +1,7 @@
 package bvanseg.kcommons.buffers
 
 import bvanseg.kcommons.projects.Version
+import org.joml.*
 import java.nio.ByteBuffer
 import java.util.*
 
@@ -19,3 +20,8 @@ fun ByteBuffer.getString(): String = String(this.getBytes())
 fun ByteBuffer.getVersion(): Version = Version(int, int, int)
 
 fun ByteBuffer.getBoolean(): Boolean  = get().toInt() == 1
+
+/** JOML Helper Functions **/
+fun ByteBuffer.getVector3i(): Vector3ic = Vector3i(this.int, this.int, this.int)
+fun ByteBuffer.getVector3f(): Vector3fc = Vector3f(this.float, this.float, this.float)
+fun ByteBuffer.getVector3d(): Vector3dc = Vector3d(this.double, this.double, this.double)
--- src/main/kotlin/bvanseg/kcommons/simplenet/packet/PacketExtensions.kt
+++ src/main/kotlin/bvanseg/kcommons/simplenet/packet/PacketExtensions.kt
@@ -2,6 +2,9 @@ package bvanseg.kcommons.simplenet.packet
 
 import bvanseg.kcommons.projects.Version
 import com.github.simplenet.packet.Packet
+import org.joml.Vector3dc
+import org.joml.Vector3fc
+import org.joml.Vector3ic
 import java.util.*
 
 /**
@@ -18,3 +21,8 @@ fun Packet.putVersion(version: Version) {
     putInt(version.minor)
     putInt(version.patch)
 }
+
+/** JOML Helper Functions **/
+fun Packet.putVector3i(vector3ic: Vector3ic): Packet = this.putInt(vector3ic.x()).putInt(vector3ic.y()).putInt(vector3ic.z())
+fun Packet.putVector3f(vector3fc: Vector3fc): Packet = this.putFloat(vector3fc.x()).putFloat(vector3fc.y()).putFloat(vector3fc.z())
+fun Packet.putVector3d(vector3dc: Vector3dc): Packet = this.putDouble(vector3dc.x()).putDouble(vector3dc.y()).putDouble(vector3dc.z())
