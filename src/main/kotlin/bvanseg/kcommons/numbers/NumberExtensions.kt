8669a3bd63bdbe446b8553076d2114e4b142b18f
bvanseg <bvanseg@gmail.com>
2019-10-09 15:08:20 -0400

    Added number formatting.

--- src/main/kotlin/bvanseg/kcommons/numbers/NumberExtensions.kt
+++ src/main/kotlin/bvanseg/kcommons/numbers/NumberExtensions.kt
@@ -1,5 +1,15 @@
 package bvanseg.kcommons.numbers
 
+import java.text.NumberFormat
+
+
+private val numberFormat = NumberFormat.getNumberInstance()
+
+/**
+ * Formats this number to a [String] with digit grouping
+ */
+fun Number.format(): String = numberFormat.format(this)
+
 fun <T: Number> Number.clamp(min: T, max: T): T = when {
     this.toDouble() < min.toDouble() -> min
     this.toDouble() > max.toDouble() -> max
