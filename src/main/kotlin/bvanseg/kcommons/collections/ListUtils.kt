984d90ed808fab268ea295e6e1a8079f949eb5b2
bvanseg <bvanseg@gmail.com>
2019-10-17 20:38:11 -0400

    Added cartesian product function.

---
+++ src/main/kotlin/bvanseg/kcommons/collections/ListUtils.kt
@@ -0,0 +1,24 @@
+package bvanseg.kcommons.collections
+
+import java.util.ArrayList
+
+
+fun <T> cartesianProduct(lists: List<List<T>>): List<List<T>> {
+    val resultLists = ArrayList<List<T>>()
+    if (lists.isEmpty()) {
+        resultLists.add(ArrayList())
+        return resultLists
+    } else {
+        val firstList = lists[0]
+        val remainingLists = cartesianProduct(lists.subList(1, lists.size))
+        for (condition in firstList) {
+            for (remainingList in remainingLists) {
+                val resultList = ArrayList<T>()
+                resultList.add(condition)
+                resultList.addAll(remainingList)
+                resultLists.add(resultList)
+            }
+        }
+    }
+    return resultLists
+}
