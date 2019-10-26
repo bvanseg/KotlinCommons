f1baead1515ab81d3bf472e68f94769527ccb6f8
bvanseg <bvanseg@gmail.com>
2019-10-26 12:03:18 -0400

    [1.0.1] Added a KotlinCommons object for holding metadata.

---
+++ src/main/kotlin/bvanseg/kcommons/KotlinCommons.kt
@@ -0,0 +1,17 @@
+package bvanseg.kcommons
+
+import bvanseg.kcommons.projects.Version
+
+/**
+ * An object representing KotlinCommons, storing miscellaneous data about the overall project.
+ *
+ * @author Boston Vanseghi
+ * @since 1.0.1
+ */
+object KotlinCommons {
+
+    /**
+     * The version of KotlinCommons.
+     */
+    val VERSION = Version("1.0.1")
+}
