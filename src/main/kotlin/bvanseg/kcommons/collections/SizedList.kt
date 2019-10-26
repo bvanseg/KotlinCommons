167a8972391359e89e279b489433c16573b373e9
bvanseg <bvanseg@gmail.com>
2019-10-26 12:16:33 -0400

    [1.0.1] Added SizedList.

---
+++ src/main/kotlin/bvanseg/kcommons/collections/SizedList.kt
@@ -0,0 +1,23 @@
+package bvanseg.kcommons.collections
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+/**
+ * A very minimal class that limits the number of elements that can be added to it.
+ *
+ * @author bright_spark
+ * @since 1.0.1
+ */
+class SizedList<T>(private val maxSize: Int): LinkedList<T>() {
+
+    /**
+     * Adds an element to the list. If the current size of the list exceeds the max size, the head element of the list
+     * will be dropped.
+     */
+    override fun add(element: T): Boolean {
+        if (this.size == maxSize)
+            this.removeAt(0)
+        return super.add(element)
+    }
+}
