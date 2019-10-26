2460f2c474ea237e366104299b2adce319464e06
bvanseg <bvanseg@gmail.com>
2019-10-26 12:00:07 -0400

    [1.0.1] Reclassified clamps as comparable extensions and added docs.

--- src/main/kotlin/bvanseg/kcommons/numbers/NumberExtensions.kt
+++ src/main/kotlin/bvanseg/kcommons/numbers/NumberExtensions.kt
@@ -6,19 +6,6 @@ import java.text.NumberFormat
 private val numberFormat = NumberFormat.getNumberInstance()
 
 /**
- * Formats this number to a [String] with digit grouping
+ * Formats this number to a [String] with digit grouping.
  */
 fun Number.format(): String = numberFormat.format(this)
-
-fun <T : Comparable<T>> T.clamp(min: T, max: T): T = when {
-    this < min -> min
-    this > max -> max
-    else -> this
-}
-
-fun <T : Comparable<T>> T?.clampOrNull(min: T, max: T): T? = when {
-    this == null -> null
-    this < min -> min
-    this > max -> max
-    else -> this
-}
--- src/main/kotlin/bvanseg/kcommons/numbers/NumberUtils.kt
+++ src/main/kotlin/bvanseg/kcommons/numbers/NumberUtils.kt
@@ -1,14 +1,1 @@
 package bvanseg.kcommons.numbers
-
-fun <T : Comparable<T>> clamp(value: T, min: T, max: T): T = when {
-    value < min -> min
-    value > max -> max
-    else -> value
-}
-
-fun <T : Comparable<T>> clampOrNull(value: T?, min: T, max: T): T? = when {
-    value == null -> null
-    value < min -> min
-    value > max -> max
-    else -> value
-}
