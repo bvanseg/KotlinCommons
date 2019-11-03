4125c87a088d6472ce2ce9d8a7b1d3e9da750558
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2019-11-03 12:34:24 -0500

    Cleaned up compression testing.

--- src/test/kotlin/bvanseg/kotlincommons/compression/CompressionTest.kt
+++ src/test/kotlin/bvanseg/kotlincommons/compression/CompressionTest.kt
@@ -11,20 +11,12 @@ internal class CompressionTest {
 	@Test
 	fun test() {
 		val ubj = UBJ()
-
 		val data = ByteArray(10000)
-		println(data.size)
 		val comp = compress(data)
-		println(comp.size)
 
 		ubj.setByteArray("bytes", *comp)
-
 		ubj.getByteArray("bytes")?.let {
-
-			println(it.size)
 			val decomp = decompress(it)
-			println(decomp.size)
-
 			assert(decomp.contentEquals(data))
 		}
 	}
