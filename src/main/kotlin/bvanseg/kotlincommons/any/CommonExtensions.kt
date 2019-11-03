2e141217e8322d5d0ec0b58888ba56459bd4991b
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2019-11-03 12:33:08 -0500

    [2.0.1] Added Observables.

--- src/main/kotlin/bvanseg/kotlincommons/any/CommonExtensions.kt
+++ src/main/kotlin/bvanseg/kotlincommons/any/CommonExtensions.kt
@@ -1,5 +1,6 @@
 package bvanseg.kotlincommons.any
 
+import bvanseg.kotlincommons.observable.Observable
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -7,3 +8,6 @@ import org.slf4j.LoggerFactory
  * Allows any class to get a logger of itself.
  */
 fun Any.getLogger(): Logger = LoggerFactory.getLogger(this::class.java)
+
+
+fun Any.toObservable(): Observable<Any> = Observable(this)
---
+++ src/test/kotlin/bvanseg/kotlincommons/observable/ObservableTest.kt
@@ -0,0 +1,63 @@
+package bvanseg.kotlincommons.observable
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+/**
+ * @author bright_spark
+ */
+internal class ObservableTest {
+    companion object {
+        private const val value1 = "Hello World"
+        private const val value2 = "Hello Realm"
+    }
+
+    @Test
+    fun get() {
+        // Given
+        val observable = Observable(value1)
+
+        // When
+        val value = observable.get()
+
+        // Then
+        assertEquals(value1, value)
+    }
+
+    @Test
+    fun set() {
+        // Given
+        val observable = Observable(value1)
+
+        // When
+        observable.set(value2)
+
+        // Then
+        assertEquals(value2, observable.get())
+    }
+
+    @Test
+    fun getCallback() {
+        // Given
+        val observable = Observable(value1)
+        observable.getCallback = { assertEquals(value1, it) }
+
+        // When
+        // Then
+        observable.get()
+    }
+
+    @Test
+    fun setCallback() {
+        // Given
+        val observable = Observable(value1)
+        observable.setCallback = { old, new ->
+            assertEquals(value1, old)
+            assertEquals(value2, new)
+        }
+
+        // When
+        // Then
+        observable.set(value2)
+    }
+}
