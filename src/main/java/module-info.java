9ad2275a7e30765c07a95a04ad7bae4e74415bd5
bvanseg <bvanseg@gmail.com>
2019-11-05 00:09:27 -0500

    Documentation and final adjustments.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -17,20 +17,25 @@ module kotlincommons {
 
     exports bvanseg.kotlincommons.any;
     exports bvanseg.kotlincommons.assets;
+    exports bvanseg.kotlincommons.booleans;
     exports bvanseg.kotlincommons.buffers;
-    exports bvanseg.kotlincommons.bytes;
     exports bvanseg.kotlincommons.classes;
     exports bvanseg.kotlincommons.collections;
     exports bvanseg.kotlincommons.comparable;
     exports bvanseg.kotlincommons.compression;
+    exports bvanseg.kotlincommons.files;
     exports bvanseg.kotlincommons.joml.vectors;
     exports bvanseg.kotlincommons.kclasses;
     exports bvanseg.kotlincommons.logging;
+    exports bvanseg.kotlincommons.measurement;
     exports bvanseg.kotlincommons.numbers;
+    exports bvanseg.kotlincommons.observable;
     exports bvanseg.kotlincommons.projects;
     exports bvanseg.kotlincommons.simplenet.packet;
+    exports bvanseg.kotlincommons.streams;
     exports bvanseg.kotlincommons.string;
     exports bvanseg.kotlincommons.system;
     exports bvanseg.kotlincommons.time;
+    exports bvanseg.kotlincommons.tuples;
     exports bvanseg.kotlincommons.ubjson;
 }
--- src/main/kotlin/bvanseg/kotlincommons/collections/MapExtensions.kt
+++ src/main/kotlin/bvanseg/kotlincommons/collections/MapExtensions.kt
@@ -4,6 +4,8 @@ package bvanseg.kotlincommons.collections
  * Reverses the [Map], swapping keys with values and values with keys.
  *
  * @return A [Map] view with type args <V, K> instead of <K, V>.
+ *
+ * @author Boston Vanseghi
  * @since 2.0.1
  */
 fun <K, V> Map<K, V>.reversed(): Map<V, K> = ReverseMap(this)
@@ -12,6 +14,8 @@ fun <K, V> Map<K, V>.reversed(): Map<V, K> = ReverseMap(this)
  * Reverses the [MutableMap], swapping keys with values and values with keys.
  *
  * @return A [MutableMap] view of the given [MutableMap] with type args <V, K> instead of <K, V>.
+ *
+ * @author Boston Vanseghi
  * @since 2.0.1
  */
 fun <K, V> MutableMap<K, V>.reversedMutable(): MutableMap<V, K> = ReverseMutableMap(this)
