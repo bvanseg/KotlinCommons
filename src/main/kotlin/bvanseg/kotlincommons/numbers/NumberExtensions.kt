af530abf26143f91b24a6c5f5795c524381f0e6e
bvanseg <bvanseg@gmail.com>
2019-11-05 00:06:04 -0500

    [2.0.1] Added isEven number extension.

--- src/main/kotlin/bvanseg/kotlincommons/numbers/NumberExtensions.kt
+++ src/main/kotlin/bvanseg/kotlincommons/numbers/NumberExtensions.kt
@@ -9,3 +9,13 @@ private val numberFormat = NumberFormat.getNumberInstance()
  * Formats this number to a [String] with digit grouping.
  */
 fun Number.format(): String = numberFormat.format(this)
+
+/**
+ * Checks whether the number is even or not.
+ *
+ * @return True or false depending on if the [Number] is even or not.
+ *
+ * @author Boston Vanseghi
+ * @since 2.0.1
+ */
+fun Number.isEven(): Boolean = this.toDouble() % 2 == 0.0
