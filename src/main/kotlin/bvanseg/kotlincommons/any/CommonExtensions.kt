797fd9d2cee85138edf696921a03c0ca2c100352
bvanseg <bvanseg@gmail.com>
2019-11-05 00:08:20 -0500

    [2.0.1] Added toOptional extension for any object.

--- src/main/kotlin/bvanseg/kotlincommons/any/CommonExtensions.kt
+++ src/main/kotlin/bvanseg/kotlincommons/any/CommonExtensions.kt
@@ -3,11 +3,25 @@ package bvanseg.kotlincommons.any
 import bvanseg.kotlincommons.observable.Observable
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.util.*
 
 /**
  * Allows any class to get a logger of itself.
  */
 fun Any.getLogger(): Logger = LoggerFactory.getLogger(this::class.java)
 
-
+/**
+ * Converts any Object to an [Observable].
+ *
+ * @author Boston Vanseghi
+ * @since 2.0.1
+ */
 fun Any.toObservable(): Observable<Any> = Observable(this)
+
+/**
+ * Converts any Object to an [Optional].
+ *
+ * @author Boston Vanseghi
+ * @since 2.0.1
+ */
+fun Any.toOptional(): Optional<Any> = Optional.of(this)
