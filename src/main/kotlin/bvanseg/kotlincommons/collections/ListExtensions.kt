a9a4fbca85d83dc0697641d8a958639e9cac7ee4
bvanseg <bvanseg@gmail.com>
2019-11-05 00:09:04 -0500

    [2.0.1] Added addAll collection extension variant that takes varargs.

--- src/main/kotlin/bvanseg/kotlincommons/collections/ListExtensions.kt
+++ src/main/kotlin/bvanseg/kotlincommons/collections/ListExtensions.kt
@@ -2,9 +2,20 @@ package bvanseg.kotlincommons.collections
 
 fun <E> List<E>.containsAny(list: List<E>): Boolean {
     list.forEach {
-        if (this.contains(it)) {
+        if (this.contains(it))
             return true
-        }
     }
     return false
 }
+
+/**
+ * Adds a vararg collection of [elements] to the [MutableCollection].
+ *
+ * @param elements - The elements to add to the collection.
+ *
+ * @return Whether or not adding all of the elements succeeded.
+ *
+ * @author Boston Vanseghi
+ * @since 2.0.1
+ */
+fun <E> MutableCollection<E>.addAll(vararg elements: E): Boolean = this.addAll(*elements)
