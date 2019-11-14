a93ead0bcffd914420b97a952d0898cb6a587090
bvanseg <bvanseg@gmail.com>
2019-11-14 12:33:28 -0500

    [2.1.2] Added @Beta annotation.

---
+++ src/main/kotlin/bvanseg/kotlincommons/annotations/Annotations.kt
@@ -0,0 +1,9 @@
+package bvanseg.kotlincommons.annotations
+
+/**
+ * Designates a certain piece of code as unstable and subject to change.
+ *
+ * @author Boston Vanseghi.
+ * @since 2.1.2
+ */
+annotation class Beta
