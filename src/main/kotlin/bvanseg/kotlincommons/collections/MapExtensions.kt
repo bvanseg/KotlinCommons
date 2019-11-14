669cb4d5e98abb62eea3d1f9640ff57be49cfcff
bvanseg <bvanseg@gmail.com>
2019-11-14 12:18:08 -0500

    [2.1.2] Reworked maps. See description.
    - Removed ReverseMap.
    - Removed ReverseMutableMap.
    - Removed DuelMutableMap.
    - Added DualHashMap to replace the removed maps.
    - Extension functions to reverse maps have been replaced with toDualMap().

--- src/main/kotlin/bvanseg/kotlincommons/collections/MapExtensions.kt
+++ src/main/kotlin/bvanseg/kotlincommons/collections/MapExtensions.kt
@@ -24,21 +24,9 @@
 package bvanseg.kotlincommons.collections
 
 /**
- * Reverses the [Map], swapping keys with values and values with keys.
- *
- * @return A [Map] view with type args <V, K> instead of <K, V>.
+ * Takes a [HashMap] and creates a [DualHashMap] with it, allowing it to be reversed.
  *
  * @author Boston Vanseghi
- * @since 2.0.1
+ * @since 2.1.2
  */
-fun <K, V> Map<K, V>.reversed(): Map<V, K> = ReverseMap(this)
-
-/**
- * Reverses the [MutableMap], swapping keys with values and values with keys.
- *
- * @return A [MutableMap] view of the given [MutableMap] with type args <V, K> instead of <K, V>.
- *
- * @author Boston Vanseghi
- * @since 2.0.1
- */
-fun <K, V> MutableMap<K, V>.reversedMutable(): MutableMap<V, K> = ReverseMutableMap(this)
+fun <K, V> HashMap<K, V>.toDualMap(): DualHashMap<K, V> = DualHashMap(this)
