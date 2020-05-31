05c38d8ffb001597530bee2fbc884e04a4017788
bvanseg <bvanseg@gmail.com>
2020-05-30 23:41:01 -0400

    [2.2.4] Testing optional parameter fix.

--- src/main/kotlin/bvanseg/kotlincommons/armada/commands/InternalCommand.kt
+++ src/main/kotlin/bvanseg/kotlincommons/armada/commands/InternalCommand.kt
@@ -329,12 +329,13 @@ open class InternalCommand(
             for(paramName in params.keys)
             {
                 val workingParameterValue = (params[paramName] ?: Unit)::class
+                val workingParameter = allParameters.find { it.name == paramName }
                 val expectedParameterType = allParameters.find { it.name == paramName }!!.type.getKClass()
 
                 if(workingParameterValue != expectedParameterType)
                     invalidParameters.add("$paramName (expected: ${expectedParameterType.simpleName})")
 
-                if(invalidParameters.isNotEmpty())
+                if(invalidParameters.isNotEmpty() || (workingParameter != null && workingParameter.isOptional && workingParameterValue != Unit::class))
                     throw InvalidParameterException("Invalid types for given parameter${if(invalidParameters.size > 1) "s" else ""}: ${invalidParameters.joinToString(", ")}")
             }
 
