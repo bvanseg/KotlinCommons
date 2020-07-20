aa619625e10cc1c0c8faaa16d12cf9e35a827d8e
Alex Couch <alcouch65@gmail.com>
2020-07-19 22:49:37 -0700

    Added a pretty printer DSL which provides easily maintainable, easily read, pretty strings. It provides indentation tracking, ansi codes for styling and coloring, and dynamic spacing. This could be abstracted in the future for allowing customizations depending on what kind of strings we are building, either raw strings, or writing to streams like sockets and files, logging, or async purposes.

---
+++ src/test/kotlin/bvanseg/kotlincommons/PrettyPrinter.kt
@@ -0,0 +1,72 @@
+package bvanseg.kotlincommons
+
+import bvanseg.kotlincommons.prettyprinter.PrettyColors
+import bvanseg.kotlincommons.prettyprinter.buildPrettyString
+import org.junit.jupiter.api.Test
+
+class PrettyPrinterTest{
+    @ExperimentalStdlibApi
+    @Test
+    fun appendTest(){
+        val string = buildPrettyString {
+            append("Hello, world!")
+        }
+        println(string)
+    }
+
+    @ExperimentalStdlibApi
+    @Test
+    fun appendWithNewLineTest(){
+        val string = buildPrettyString {
+            appendWithNewLine("Hello, world!")
+        }
+        println(string)
+    }
+
+    @ExperimentalStdlibApi
+    @Test
+    fun simpleColorTest(){
+        val string = buildPrettyString {
+            red{
+                appendWithNewLine("Hello, world!")
+            }
+            blue{
+                appendWithNewLine("Hello, world!")
+            }
+            green{
+                appendWithNewLine("Hello, world!")
+            }
+            yellow{
+                appendWithNewLine("Hello, world!")
+            }
+            white{
+                appendWithNewLine("Hello, world!")
+            }
+            black{
+                appendWithNewLine("Hello, world!")
+            }
+            underline{
+                appendWithNewLine("Hello, world!")
+            }
+            bold{
+                appendWithNewLine("Hello, world!")
+            }
+            italics{
+                appendWithNewLine("Hello, world!")
+            }
+        }
+        val target =
+            "${PrettyColors.RED.ansi}Hello, world!\n${PrettyColors.RESET.ansi}" +
+            "${PrettyColors.BLUE.ansi}Hello, world!\n${PrettyColors.RESET.ansi}" +
+            "${PrettyColors.GREEN.ansi}Hello, world!\n${PrettyColors.RESET.ansi}" +
+            "${PrettyColors.YELLOW.ansi}Hello, world!\n${PrettyColors.RESET.ansi}" +
+            "${PrettyColors.WHITE.ansi}Hello, world!\n${PrettyColors.RESET.ansi}" +
+            "${PrettyColors.BLACK.ansi}Hello, world!\n${PrettyColors.RESET.ansi}" +
+            "${PrettyColors.UNDERLINE.ansi}Hello, world!\n${PrettyColors.RESET.ansi}" +
+            "${PrettyColors.BOLD.ansi}Hello, world!\n${PrettyColors.RESET.ansi}" +
+            "${PrettyColors.ITALIC.ansi}Hello, world!\n${PrettyColors.RESET.ansi}"
+        println(string)
+        println(target)
+        assert(string == target)
+    }
+}
