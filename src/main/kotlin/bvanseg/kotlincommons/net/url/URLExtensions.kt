539dc9829e633c6219a2329434334eaede9e1823
Jacob Glickman <jhg023@bucknell.edu>
2020-07-29 11:28:47 -0400

    [2.4.0-alpha1] Don't remove `/` on non-Windows OS.
    Signed-off-by: Jacob Glickman <jhg023@bucknell.edu>

--- src/main/kotlin/bvanseg/kotlincommons/net/url/URLExtensions.kt
+++ src/main/kotlin/bvanseg/kotlincommons/net/url/URLExtensions.kt
@@ -4,10 +4,13 @@ import bvanseg.kotlincommons.string.remove
 import java.net.URL
 
 /**
- * Returns the URL without the file:/ protocol, if it has it TODO: Remove arbitrary starting
- * protocol
+ * Returns the URL without the file:/ protocol, if it has it
+ *
+ * TODO: Remove arbitrary starting protocol
  *
  * @author CraftSpider
  * @since 2.0.0
  */
-fun URL.toPath(): String = toString().remove("file:/")
+fun URL.toPath(): String =
+    if (System.getProperty("os.name").contains("Windows")) toString().remove("file:/")
+    else toString().remove("file:")
