2c82003eea86fb8506a37dcf106b9cf48833dce9
bvanseg <bvanseg@gmail.com>
2020-09-08 07:55:17 -0400

    [2.4.0] Updated copyright to 2020.

--- src/main/kotlin/bvanseg/kotlincommons/fp/Kinds.kt
+++ src/main/kotlin/bvanseg/kotlincommons/fp/Kinds.kt
@@ -1,6 +1,48 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Boston Vanseghi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package bvanseg.kotlincommons.fp
 
+/**
+ * @author Alex Couch
+ * @since 2.4.0
+ */
 interface Kind<out F, out A>
+
+/**
+ * @author Alex Couch
+ * @since 2.4.0
+ */
 typealias Kind2<F, A, B> = Kind<Kind<F, A>, B>
+
+/**
+ * @author Alex Couch
+ * @since 2.4.0
+ */
 typealias Kind3<F, A, B, C> = Kind<Kind2<F, A, B>, C>
+
+/**
+ * @author Alex Couch
+ * @since 2.4.0
+ */
 typealias Kind4<F, A, B, C, D> = Kind<Kind3<F, A, B, C>, D>
