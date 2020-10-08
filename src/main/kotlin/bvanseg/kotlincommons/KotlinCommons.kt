efd5ed7a82f6c2197a14c701346b3f01d6c281a0
bvanseg <bvanseg@gmail.com>
2020-10-08 00:41:37 -0400

    Version bump.

--- src/main/kotlin/bvanseg/kotlincommons/KotlinCommons.kt
+++ src/main/kotlin/bvanseg/kotlincommons/KotlinCommons.kt
@@ -37,7 +37,7 @@ object KotlinCommons {
     /**
      * The version of KotlinCommons.
      */
-    val VERSION = Version(2, 6, 0)
+    val VERSION = Version(2, 6, 1)
 
     val KC_LOGGER = getLogger()
 }
