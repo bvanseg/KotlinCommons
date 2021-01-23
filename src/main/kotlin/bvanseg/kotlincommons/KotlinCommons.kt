cc96d251da2a01cd965f3d4ae31aa066e77a5510
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2021-01-23 17:26:57 -0500

    Format strings for SLF4J extension functions.

--- src/main/kotlin/bvanseg/kotlincommons/KotlinCommons.kt
+++ src/main/kotlin/bvanseg/kotlincommons/KotlinCommons.kt
@@ -37,7 +37,7 @@ object KotlinCommons {
     /**
      * The version of KotlinCommons.
      */
-    val VERSION = Version(2, 7, 0, "alpha14")
+    val VERSION = Version(2, 7, 0, "alpha15")
 
     val KC_LOGGER = getLogger()
 }
--- src/main/kotlin/bvanseg/kotlincommons/logging/LoggerExtensions.kt
+++ src/main/kotlin/bvanseg/kotlincommons/logging/LoggerExtensions.kt
@@ -25,8 +25,8 @@ package bvanseg.kotlincommons.logging
 
 import org.slf4j.Logger
 
-fun Logger.debug(any: Any?) = this.debug(any.toString())
-fun Logger.error(any: Any?) = this.error(any.toString())
-fun Logger.info(any: Any?) = this.info(any.toString())
-fun Logger.trace(any: Any?) = this.trace(any.toString())
-fun Logger.warn(any: Any?) = this.warn(any.toString())
+fun Logger.debug(any: Any?) = this.debug("{}", any.toString())
+fun Logger.error(any: Any?) = this.error("{}", any.toString())
+fun Logger.info(any: Any?) = this.info("{}", any.toString())
+fun Logger.trace(any: Any?) = this.trace("{}", any.toString())
+fun Logger.warn(any: Any?) = this.warn("{}", any.toString())
