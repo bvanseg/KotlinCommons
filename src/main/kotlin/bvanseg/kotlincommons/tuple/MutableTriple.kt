0c08d6084f806f72dfcb05c626f6b34592f09272
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2021-01-24 00:03:09 -0500

    Added MutableTriple class.

---
+++ src/main/kotlin/bvanseg/kotlincommons/tuple/MutableTriple.kt
@@ -0,0 +1,40 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Boston Vanseghi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package bvanseg.kotlincommons.tuple
+
+/**
+ * A mutable version of the [Triple] class.
+ *
+ * @author Boston Vanseghi
+ * @since 2.7.0
+ */
+data class MutableTriple<A, B, C>(var first: A, var second: B, var third: C) {
+    override fun toString(): String = "($first, $second, $third)"
+
+    fun setAll(first: A, second: B, third: C) = synchronized(this) {
+        this.first = first
+        this.second = second
+        this.third = third
+    }
+}
