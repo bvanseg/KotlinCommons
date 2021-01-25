7cddf2751826c74dd4dfa119337e1e2ca11983cd
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2021-01-25 04:40:56 -0500

    Moved type package under reflect package.

--- src/main/kotlin/bvanseg/kotlincommons/reflect/type/TypeReference.kt
+++ src/main/kotlin/bvanseg/kotlincommons/reflect/type/TypeReference.kt
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package bvanseg.kotlincommons.type
+package bvanseg.kotlincommons.reflect.type
 
 import bvanseg.kotlincommons.project.Experimental
 import java.lang.reflect.ParameterizedType
