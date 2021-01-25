cf700dbade0363114b558e987359ef0826a32366
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2021-01-25 00:29:02 -0500

    Added Experimental annotation.

---
+++ src/main/kotlin/bvanseg/kotlincommons/project/Experimental.kt
@@ -0,0 +1,9 @@
+package bvanseg.kotlincommons.project
+
+/**
+ * Classifies a construct as experimental. Experimental constructs should be used with caution.
+ *
+ * @author Boston Vanseghi
+ * @since 2.7.0
+ */
+annotation class Experimental()
