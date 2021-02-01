02a210cc1d919084e4a166e1e210bc4c614ac820
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2021-01-31 19:58:51 -0500

    Refactored TimeAPI.kt to KTimeAPI.kt.

--- src/main/kotlin/bvanseg/kotlincommons/KotlinCommons.kt
+++ src/main/kotlin/bvanseg/kotlincommons/KotlinCommons.kt
@@ -59,7 +59,7 @@ object KotlinCommons {
     /**
      * The version of KotlinCommons.
      */
-    val VERSION = Version(2, 7, 0)
+    val VERSION = Version(2, 7, 1)
 
     /**
      * A default logger provided by KotlinCommons.
