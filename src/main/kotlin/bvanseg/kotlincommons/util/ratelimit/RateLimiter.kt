24cc6d43d18fc6d36bfab984106ab2a0e6117b69
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2021-01-31 23:25:25 -0500

    Update RateLimiter.kt

--- src/main/kotlin/bvanseg/kotlincommons/util/ratelimit/RateLimiter.kt
+++ src/main/kotlin/bvanseg/kotlincommons/util/ratelimit/RateLimiter.kt
@@ -88,7 +88,7 @@ class RateLimiter constructor(
                             val snapshotMillis = System.currentTimeMillis() % tokenBucket.refillTime
                             val delta = tokenBucket.refillTime - snapshotMillis
                             nextRefreshTime = System.currentTimeMillis() + delta
-                            logger.debug { "Calculated time until next TokenBucket refill is $nextRefreshTime." }
+                            logger.debug { "Calculated time until next TokenBucket refill is $delta." }
                         } catch (e: Exception) {
                             exceptionStrategy(e)
                         }
