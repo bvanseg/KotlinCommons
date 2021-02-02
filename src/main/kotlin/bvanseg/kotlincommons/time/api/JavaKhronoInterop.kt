1a4fe46bd64cfcb42480106e0db9fc899d01d036
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2021-02-02 13:15:49 -0500

    Added extension functions to convert LocalDate, LocalTime, and LocalDateTime to khrono types.

---
+++ src/main/kotlin/bvanseg/kotlincommons/time/api/JavaKhronoInterop.kt
@@ -0,0 +1,34 @@
+package bvanseg.kotlincommons.time.api
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+
+fun LocalDate.toKhronoDate(): KhronoDate = this.run {
+    KhronoDate(
+        d = this.dayOfMonth.toDouble(),
+        mth = this.monthValue,
+        yr = this.year.toDouble()
+    )
+}
+
+fun LocalTime.toKhronoTime(): KhronoTime = this.run {
+    KhronoTime(
+        hr = this.hour.toDouble(),
+        min = this.minute.toDouble(),
+        sec = this.second.toDouble(),
+        nano = this.nano.toDouble()
+    )
+}
+
+fun LocalDateTime.toKhronoDateTime(): KhronoDateTime = this.run {
+    KhronoDateTime(
+        day = this.dayOfMonth.toDouble(),
+        month = this.monthValue,
+        year = this.year.toDouble(),
+        hour = this.hour.toDouble(),
+        minute = this.minute.toDouble(),
+        second = this.second.toDouble(),
+        nanosecond = this.nano.toDouble()
+    )
+}
