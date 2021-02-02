ab2195cf0070e89af7a45761f5be1bef3f78705e
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2021-02-02 00:59:44 -0500

    Added additional thread extensions for KhronoTime, KhronoDate, and KhronoDateTime.

--- src/main/kotlin/bvanseg/kotlincommons/lang/thread/ThreadExtensions.kt
+++ src/main/kotlin/bvanseg/kotlincommons/lang/thread/ThreadExtensions.kt
@@ -1,5 +1,11 @@
 package bvanseg.kotlincommons.lang.thread
 
 import bvanseg.kotlincommons.time.api.Khrono
+import bvanseg.kotlincommons.time.api.KhronoDate
+import bvanseg.kotlincommons.time.api.KhronoDateTime
+import bvanseg.kotlincommons.time.api.KhronoTime
 
 fun Thread.sleep(time: Khrono) = Thread.sleep(time.toMillis().toLong())
+fun Thread.sleep(time: KhronoTime) = Thread.sleep(time.asMillis.toLong())
+fun Thread.sleep(date: KhronoDate) = Thread.sleep(date.asMillis.toLong())
+fun Thread.sleep(dateTime: KhronoDateTime) = Thread.sleep(dateTime.asMillis.toLong())
