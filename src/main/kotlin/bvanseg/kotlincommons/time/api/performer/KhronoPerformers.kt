6ee80421553007a2639f9b176fcec829fa367355
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2021-02-02 01:24:07 -0500

    Added additional KhronoPerformer every functions.

--- src/main/kotlin/bvanseg/kotlincommons/time/api/performer/KhronoPerformers.kt
+++ src/main/kotlin/bvanseg/kotlincommons/time/api/performer/KhronoPerformers.kt
@@ -1,6 +1,9 @@
 package bvanseg.kotlincommons.time.api.performer
 
 import bvanseg.kotlincommons.time.api.Khrono
+import bvanseg.kotlincommons.time.api.KhronoDate
+import bvanseg.kotlincommons.time.api.KhronoDateTime
+import bvanseg.kotlincommons.time.api.KhronoTime
 import bvanseg.kotlincommons.time.api.milliseconds
 
 fun every(timeInMillis: Long, counterDrift: Boolean = false, cb: (KhronoPerformer) -> Unit): KhronoPerformer =
@@ -8,3 +11,12 @@ fun every(timeInMillis: Long, counterDrift: Boolean = false, cb: (KhronoPerforme
 
 fun every(frequency: Khrono, counterDrift: Boolean = false, cb: (KhronoPerformer) -> Unit): KhronoPerformer =
     KhronoPerformer(frequency, cb, counterDrift)
+
+fun every(frequency: KhronoTime, counterDrift: Boolean = false, cb: (KhronoPerformer) -> Unit): KhronoPerformer =
+    every(frequency.asMillis.toLong(), counterDrift, cb)
+
+fun every(frequency: KhronoDate, counterDrift: Boolean = false, cb: (KhronoPerformer) -> Unit): KhronoPerformer =
+    every(frequency.asMillis.toLong(), counterDrift, cb)
+
+fun every(frequency: KhronoDateTime, counterDrift: Boolean = false, cb: (KhronoPerformer) -> Unit): KhronoPerformer =
+    every(frequency.asMillis.toLong(), counterDrift, cb)
