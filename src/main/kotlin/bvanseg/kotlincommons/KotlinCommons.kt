27f57bfa26b23172399d69f8fdc6eb67e3fedebf
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2021-03-05 02:02:36 -0500

    Removed RateLimiter and related classes.

--- src/main/kotlin/bvanseg/kotlincommons/KotlinCommons.kt
+++ src/main/kotlin/bvanseg/kotlincommons/KotlinCommons.kt
@@ -60,7 +60,7 @@ object KotlinCommons {
     /**
      * The version of KotlinCommons.
      */
-    val VERSION = Version(2, 9, 9)
+    val VERSION = Version(2, 10, 0)
 
     /**
      * A default logger provided by KotlinCommons.
