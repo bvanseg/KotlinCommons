2ee6b5cda14e8e9dd3b67ff5151bee005238b031
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2021-03-05 01:26:30 -0500

    Version bump.

--- src/main/kotlin/bvanseg/kotlincommons/KotlinCommons.kt
+++ src/main/kotlin/bvanseg/kotlincommons/KotlinCommons.kt
@@ -60,7 +60,7 @@ object KotlinCommons {
     /**
      * The version of KotlinCommons.
      */
-    val VERSION = Version(2, 9, 8)
+    val VERSION = Version(2, 9, 9)
 
     /**
      * A default logger provided by KotlinCommons.
