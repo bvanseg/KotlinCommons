f91f58610ba3b593d67ce0dda138a3210e71c319
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2021-03-10 22:49:50 -0500

    Prefer length checking over using regex for CharTransformer.kt.

--- src/main/kotlin/bvanseg/kotlincommons/lang/command/transformer/impl/primitive/CharTransformer.kt
+++ src/main/kotlin/bvanseg/kotlincommons/lang/command/transformer/impl/primitive/CharTransformer.kt
@@ -9,7 +9,9 @@ import bvanseg.kotlincommons.lang.command.transformer.Transformer
  * @since 2.10.0
  */
 object CharTransformer: Transformer<Char>(Char::class) {
-    private val REGEX = Regex("^.$")
-    override fun matches(buffer: PeekingTokenBuffer): Boolean = buffer.peek()?.value?.matches(REGEX) ?: false
+    override fun matches(buffer: PeekingTokenBuffer): Boolean {
+        val text = buffer.peek()?.value ?: return false
+        return text.length == 1
+    }
     override fun parse(buffer: ArgumentTokenBuffer): Char = buffer.next().value[0]
 }
