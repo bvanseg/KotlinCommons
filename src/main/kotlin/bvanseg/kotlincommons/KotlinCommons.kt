b6dc2daf03c9dcb7179e0b1338298ddaf5b02cb4
Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>
2021-04-10 15:33:57 -0400

    Updated dependencies.
    Signed-off-by: Boston Vanseghi <51012994+bvanseg@users.noreply.github.com>

--- src/main/kotlin/bvanseg/kotlincommons/KotlinCommons.kt
+++ src/main/kotlin/bvanseg/kotlincommons/KotlinCommons.kt
@@ -60,7 +60,7 @@ object KotlinCommons {
     /**
      * The version of KotlinCommons.
      */
-    val VERSION = Version(2, 10, 7)
+    val VERSION = Version(2, 10, 8)
 
     /**
      * A default logger provided by KotlinCommons.
